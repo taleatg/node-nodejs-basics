04338bf4f2290117751100b9605c69d11ea2756d
taleatg <t_korbyt@mail.ru>
2022-06-01 23:38:34 +0300

    feat: add solution for File system (src/fs)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const file = path.join(__dirname, '/files/fileToRead.txt');
+
+  fs.open(file, 'r', (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        console.error(new Error('FS operation failed'));
+      }
+    }
+
+    const stream = new fs.ReadStream(file, {encoding: 'utf8'});
+    stream.pipe(process.stdout);
+  });
 };
+
+read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,25 @@
+import fs, { access, constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 export const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const file = path.join(__dirname, '/files/wrongFilename.txt');
+  const fileRename = path.join(__dirname, '/files/properFilename.md');
+
+  access(file, constants.F_OK, (err) => {
+    err
+      ? console.error(new Error('FS operation failed'))
+      : access(fileRename, constants.F_OK, (err) => {
+        err
+          ? fs.rename(file, fileRename, (err) => {
+            err ? new Error(err) : console.log('Done successfully!');
+          })
+          : console.error(new Error('FS operation failed'));
+      })
+  })
 };
+
+rename();

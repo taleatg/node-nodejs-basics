4d1100cfac80041d62468b91df9ae30ec34fa138
taleatg <t_korbyt@mail.ru>
2022-06-03 21:30:44 +0300

    feat: add solution for Streams(src/streams)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const file = path.join(__dirname, '/files/fileToWrite.txt');
+  const stream = new fs.createWriteStream(file, {encoding: 'utf8'});
+
+  process.stdin.on('data', data => {
+    stream.write(data.toString());
+  })
 };
+
+write();
